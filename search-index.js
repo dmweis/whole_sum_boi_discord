3c4166e1daba2f39b443fb5a87ed1df7ff6d0017
dmweis <dmweis@users.noreply.github.com>
2022-06-22 01:59:00 +0000

    deploy: 6a474af816dd9b2f5b5ac6577a72743321f9a5bc

---
+++ search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = JSON.parse('{\
+"whole_sum_boi_discord":{"doc":"","t":[5],"n":["main"],"q":["whole_sum_boi_discord"],"d":[""],"i":[0],"f":[[[]]],"p":[]}\
+}');
+if (window.initSearch) {window.initSearch(searchIndex)};
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["whole_sum_boi_discord"] = {"name":"","files":["main.rs"]};
+createSourceSidebar();
